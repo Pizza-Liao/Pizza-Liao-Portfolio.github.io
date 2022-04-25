d341d8342ca1cff05d519e0499b78ade7db28706
Pizza <104219554+Pizza-Liao@users.noreply.github.com>
2022-04-25 23:01:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -30,22 +30,30 @@ function pictureChange() {
 }
 
 function pictureChangeminus() {
-  pictureCount--;
-  if (pictureCount % 3 === Math.abs(1)) {
-    document.getElementById("podcast-picture").src =
-      "https://images.weserv.nl/?il&fit=contain&w=200&h=200&dpr=2&url=https://files.soundon.fm/1635399769293-5d9b69cd-6fd6-4fad-8bde-00c1a6ae8944.jpeg";
-    document.getElementById("podcast-link").href =
-      "https://player.soundon.fm/p/112db27e-1fa9-4de3-af5a-4294b2a547f9/episodes/5ed2a99b-e1c2-4c5a-8124-1e5f97316143";
-  } else if (pictureCount % 3 === 2) {
+  if (pictureCount === 0) {
+    pictureCount = pictureCount + 2;
     document.getElementById("podcast-picture").src =
       "https://images.weserv.nl/?il&fit=contain&w=200&h=200&dpr=2&url=https://files.soundon.fm/1634189147290-3245bab8-61e7-4d32-bbf2-e49a58e639e6.jpeg";
     document.getElementById("podcast-link").href =
       "https://player.soundon.fm/p/112db27e-1fa9-4de3-af5a-4294b2a547f9/episodes/00de2efd-f2a8-4ba3-895a-d082a20b0f43";
   } else {
-    document.getElementById("podcast-picture").src =
-      "https://images.weserv.nl/?il&fit=contain&w=200&h=200&dpr=2&url=https://files.soundon.fm/1636607264544-ba75f431-c575-413f-8a44-990cdcd9ed9d.jpeg";
-    document.getElementById("podcast-link").href =
-      "https://player.soundon.fm/p/112db27e-1fa9-4de3-af5a-4294b2a547f9/episodes/8e03ece0-e246-48bf-81ca-495807c0d587";
+    pictureCount--;
+    if (Math.abs(pictureCount) % 3 === Math.abs(1)) {
+      document.getElementById("podcast-picture").src =
+        "https://images.weserv.nl/?il&fit=contain&w=200&h=200&dpr=2&url=https://files.soundon.fm/1635399769293-5d9b69cd-6fd6-4fad-8bde-00c1a6ae8944.jpeg";
+      document.getElementById("podcast-link").href =
+        "https://player.soundon.fm/p/112db27e-1fa9-4de3-af5a-4294b2a547f9/episodes/5ed2a99b-e1c2-4c5a-8124-1e5f97316143";
+    } else if (Math.abs(pictureCount) % 3 === Math.abs(2)) {
+      document.getElementById("podcast-picture").src =
+        "https://images.weserv.nl/?il&fit=contain&w=200&h=200&dpr=2&url=https://files.soundon.fm/1634189147290-3245bab8-61e7-4d32-bbf2-e49a58e639e6.jpeg";
+      document.getElementById("podcast-link").href =
+        "https://player.soundon.fm/p/112db27e-1fa9-4de3-af5a-4294b2a547f9/episodes/00de2efd-f2a8-4ba3-895a-d082a20b0f43";
+    } else {
+      document.getElementById("podcast-picture").src =
+        "https://images.weserv.nl/?il&fit=contain&w=200&h=200&dpr=2&url=https://files.soundon.fm/1636607264544-ba75f431-c575-413f-8a44-990cdcd9ed9d.jpeg";
+      document.getElementById("podcast-link").href =
+        "https://player.soundon.fm/p/112db27e-1fa9-4de3-af5a-4294b2a547f9/episodes/8e03ece0-e246-48bf-81ca-495807c0d587";
+    }
   }
 }
 
